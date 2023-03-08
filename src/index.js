5cb97089fce7d0677c5e724b5b29eabb588c84a5
Tamás József Simon <simont@infomix.hu>
2023-03-08 14:18:48 +0100

    step 2 API and dependecy initialization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("hello world 2");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+//define the express app
+const app = express();
+
+//add an array as a datastore (temp solution)
+const ads= [
+    {title:"Hello world 2!"}
+];
+
+//adding helmet to enchase your rest Api security
+app.use(helmet());
+
+//adding bodyparser to parse json bodies into js objects
+app.use(bodyParser.json());
+
+//enabling CORS for all requests
+app.use(cors());
+
+//define a get endpoint to return all ads
+app.get('/',(req,res) => {
+    console.log("request is responded");
+    res.send(ads);
+});
+
+//starting the server
+
+app.listen(3001, ()=> {
+    console.log("server is listening on port 3001");
+});
