b4fafc11beb5ad64c02b52f6aaef4b00227e0e4c
Tamás József Simon <simont@infomix.hu>
2023-03-09 15:49:09 +0100

    sequelize version A (not working properly)

--- src/Models/user.ts
+++ src/Models/user.ts
@@ -1,5 +1,29 @@
-export interface User {
-    id: number;
-    firstName: string;
-    lastName: string;
+import {Model,Optional,DataTypes, Sequelize} from "sequelize";
+import DbConnection from "../Database/DbConnection";
+
+interface UserAttributes{
+    id:number;
+    loginName: string;
+    password: string;
+    email: string;
+
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
 }
+
+export interface UserInput extends Optional<UserAttributes,'id'>{}
+export interface UserOutput extends Required<UserAttributes>{}
+
+export class User extends Model<UserAttributes,UserInput> implements UserAttributes {
+    id!: number;
+    loginName!:string;
+    email!: string;
+    password!: string;
+
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+    public readonly deletedAt!: Date;
+}
+
