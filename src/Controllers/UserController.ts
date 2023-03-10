24bfc6700eda4bb712fffd90fb8ae9eb9917febb
Tamás József Simon <simont@infomix.hu>
2023-03-10 11:46:36 +0100

    Finalized User controller

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,4 +1,5 @@
 import {Request,Response} from "express";
+import { User } from "../Models/user";
 
 export class UserController{
 
@@ -8,24 +9,53 @@ export class UserController{
        
     }
 
-    listUsers(req:Request,res:Response):any{
+     async listUsers(req:Request,res:Response):Promise<Response>{
         res.setHeader('Content-Type','application/json');
-        res.status(501);
+        const users = await User.findAll();
+        return res.status(200).json(users);
     }
 
-    createUser(req:Request,res:Response):any{
-        res.status(501);
+    async createUser(req:Request,res:Response):Promise<Response>{
+        var user = <User>req.body;
+        user.id = 0; // mariadb has identity insert on by default
+        user = await User.create({...user});
+        return res.status(201).json(user);
     }
 
-    readUser(req:Request,res:Response):any{
-        res.status(501);
+    async readUser(req:Request,res:Response):Promise<Response>{
+        const {id} = req.params;
+        const user: User |null = await User.findByPk(id);
+
+        if(user !== null)
+            return res.status(200).json(user);
+        else
+            return res.status(404).end();
     }
 
-    updateUser(req:Request,res:Response):any{
-        res.status(501);
+    async updateUser(req:Request,res:Response):Promise<Response>{
+        const {id} = req.params;
+        const user = <User> req.body;
+        user.id = Number(id);
+
+        await User.update({...user},{where: {id}});
+        
+        const updatedUser :User | null = await User.findByPk(id);
+        if(updatedUser !== null)
+            return res.status(200).json(updatedUser);
+        else
+            return res.status(404).end();
+            
     }
 
-    deleteUser(req:Request,res:Response):any{
-        res.status(501);
+    async deleteUser(req:Request,res:Response):Promise<Response>{
+        const {id} = req.params;
+        const deleteUser : User | null = await User.findByPk(id);
+        if(deleteUser !== null){
+            await User.destroy({where:{id}});
+            return res.status(200).json(deleteUser);
+        }
+        else{
+            return res.status(404).end();
+        }
     }
 }
--- src/router.ts
+++ src/router.ts
@@ -7,9 +7,9 @@ export const router:Router = express.Router();
 //userController
 router.get("/api/user/list", new UserController().listUsers);
 router.post("/api/user", new UserController().createUser);
-router.get("/api/user/:userId", new UserController().readUser);
-router.put("/api/user/:userId", new UserController().updateUser);
-router.delete("/api/user/:userId", new UserController().deleteUser);
+router.get("/api/user/:id", new UserController().readUser);
+router.put("/api/user/:id", new UserController().updateUser);
+router.delete("/api/user/:id", new UserController().deleteUser);
 
 //healtcheckController
 router.get("/api/health/database",new HealtCheckController().checkDatabaseConnection);
