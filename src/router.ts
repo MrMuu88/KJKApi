d4369342e60f0592ce6c47a1f916a2d374b36966
Tamás József Simon <simont@infomix.hu>
2023-03-15 14:08:00 +0100

    updated routes

--- src/router.ts
+++ src/router.ts
@@ -10,39 +10,41 @@ import 'express-async-errors';
 export const router:Router = express.Router();
 
 //userController
-router.get("/api/user/list", new UserController().listUsers);
-router.post("/api/user", new UserController().createUser);
-router.get("/api/user/:uid", new UserController().readUser);
-router.put("/api/user/:uid", new UserController().updateUser);
-router.delete("/api/user/:uid", new UserController().deleteUser);
+router.get("/api/users", new UserController().listUsers);
+router.post("/api/users", new UserController().createUser);
+router.get("/api/users/:uid", new UserController().readUser);
+router.put("/api/users/:uid", new UserController().updateUser);
+router.delete("/api/users/:uid", new UserController().deleteUser);
 
 //ParagraphController
-router.get("/api/Paragraph/list", new ParagraphController().listParagraphs);
-router.post("/api/Paragraph", new ParagraphController().createParagraph);
-router.get("/api/Paragraph/:pid", new ParagraphController().readParagraph);
-router.put("/api/Paragraph/:pid", new ParagraphController().updateParagraph);
-router.delete("/api/Paragraph/:pid", new ParagraphController().deleteParagraph);
+router.get("/api/Paragraphs", new ParagraphController().listParagraphs);
+router.post("/api/Paragraphs", new ParagraphController().createParagraph);
+router.get("/api/Paragraphs/:pid", new ParagraphController().readParagraph);
+router.put("/api/Paragraphs/:pid", new ParagraphController().updateParagraph);
+router.delete("/api/Paragraphs/:pid", new ParagraphController().deleteParagraph);
 
 //MonsterController
-router.get("/api/Monster/list", new MonsterController().listMonsters);
-router.post("/api/Monster", new MonsterController().createMonster);
-router.get("/api/Monster/:mid", new MonsterController().readMonster);
-router.put("/api/Monster/:mid", new MonsterController().updateMonster);
-router.delete("/api/Monster/:mid", new MonsterController().deleteMonster);
+router.get("/api/monsters", (req,res) => {throw ("not implemented");});
+router.get("/api/paragraph/:pid/Monsters", new MonsterController().listMonsters);
+router.post("/api/Monster/paragraph/:pid/monsters", new MonsterController().createMonster);
+router.get("/api/Monsters/:mid", new MonsterController().readMonster);
+router.put("/api/Monsters/:mid", new MonsterController().updateMonster);
+router.delete("/api/Monsters/:mid", new MonsterController().deleteMonster);
 
 //ItemController
-router.get("/api/Item/list", new ItemController().listItems);
-router.post("/api/Item", new ItemController().createItem);
-router.get("/api/Item/:iid", new ItemController().readItem);
-router.put("/api/Item/:iid", new ItemController().updateItem);
-router.delete("/api/Item/:iid", new ItemController().deleteItem);
+router.get("/api/Items", (req,res) => {throw ("not implemented");});
+router.get("/api/Paragraph/:pid/Items", new ItemController().listItems);
+router.post("/api/Paragraph/:pid/Items", new ItemController().createItem);
+router.get("/api/Items/:iid", new ItemController().readItem);
+router.put("/api/Items/:iid", new ItemController().updateItem);
+router.delete("/api/Items/:iid", new ItemController().deleteItem);
 
 //ChoiceController
-router.get("/api/Paragraph/:pid/Choice/list", new ChoiceController().listChoices);
-router.post("/api/Paragraph/:pid/Choice", new ChoiceController().createChoice);
-router.get("/api/Paragraph/:pid/Choice/:cid", new ChoiceController().readChoice);
-router.put("/api/Paragraph/:pid/Choice/:cid", new ChoiceController().updateChoice);
-router.delete("/api/Paragraph/:pid/Choice/:cid", new ChoiceController().deleteChoice);
+router.get("/api/Paragraph/:pid/Choices", new ChoiceController().listChoices);
+router.post("/api/Paragraph/:pid/Choices", new ChoiceController().createChoice);
+router.get("/api/Choices/:cid", new ChoiceController().readChoice);
+router.put("/api/Choices/:cid", new ChoiceController().updateChoice);
+router.delete("/api/Choices/:cid", new ChoiceController().deleteChoice);
 
 
 //healtcheckController
