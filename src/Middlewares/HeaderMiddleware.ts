25033bc477341932fd9f311a50f3f17dc893be71
Tamás József Simon <simont@infomix.hu>
2023-03-22 10:57:46 +0100

    added request logging

--- src/Middlewares/HeaderMiddleware.ts
+++ src/Middlewares/HeaderMiddleware.ts
@@ -1,9 +1,13 @@
 import { Request, Response,NextFunction } from "express-serve-static-core"
 
+var reqid :number = 0;
+
 const HeaderMiddleware = (req:Request,res:Response,next:NextFunction)=>{
-    console.log("header setter called");
+    reqid++;
+    console.log( `request[${reqid}]: ${req.method} ${req.url}`);
     res.setHeader("Content-Type","application/json");
     next();
+    console.log(`request[${reqid}] responded: ${res.statusCode}`);
 }
 
 export default HeaderMiddleware;
